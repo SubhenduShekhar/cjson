96826c37158db79b8864607d26fe176e03390eaa
Gupta <shubhendushekhar.gupta@delta.com>
2023-06-01 14:54:20 +0530

    updated readme for npm package

--- tests/npm/spec/tests/spec.js
+++ tests/npm/spec/tests/spec.js
@@ -1,11 +1,10 @@
-// // import { Cjson } from "coded-json";
 const Cjson = require('coded-json').Cjson;
 const path = require('path');
 
 const filePath = path.join(__dirname, "..", "..", "..", "\\test-files\\target.cjson");
 
 describe("CJSON Test 1", () => {
-    it("I should be able to deserialize comments and imports", () => {
+    it("I should be able to deserialize imports and comments", () => {
         var cjson = new Cjson(filePath);
 
         var b = cjson.deserialize();
