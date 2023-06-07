a9c018ef39b85fa589bf808d3db7dd04733e530f
Gupta <shubhendushekhar.gupta@delta.com>
2023-06-07 16:44:37 +0530

    added utils

---
+++ python/cjson/src/utils/keywords.py
@@ -0,0 +1,6 @@
+class Keywords:
+    import_key: str = "$import \""
+    single_line_comment: str = "//"
+    multiLine_comment_start: str = "/*"
+    multiLine_comment_end: str = "*/"
+    relative_jpath: str = "$."
