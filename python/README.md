7cb8ef7cb0a0dc97d22ed4b0eeccad8315ec542c
Gupta <shubhendushekhar.gupta@delta.com>
2023-06-09 12:45:12 +0530

    push ready

---
+++ python/README.md
@@ -0,0 +1,39 @@
+<center>
+    <h1 style="color: rebeccapurple;"> { CJSON } </h1>
+    <h4> Coded Javascript Object Notation </h4>
+</center>
+
+![NPM Package](https://github.com/SubhenduShekhar/cjson/actions/workflows/npm-publish.yml/badge.svg)
+
+Why static JSON if you can utilize CJSON.
+
+<b> Create your files with extension `.cjson` to unlock all these features </b>
+
+<br>
+
+## Features
+
+- [Import multiple JSON files](#Import-multiple-JSON-files)
+- [Single/ Multiple line comments](#Single-Multiple-line-comments)
+
+
+## Import multiple JSON files
+
+You can use `$import` keyword for importing
+
+```
+{
+    "source": $import "./source.json",
+    "target": {
+        "fruit": "Apple",
+        "size": "Large",
+        "color": "Red"
+    }
+}
+```
+
+## Single/ Multiple line comments
+
+For single line comments, use `//`
+
+For multi line comments, use `/*` to start, `*/` to end
--- python/setup.py
+++ python/setup.py
@@ -4,18 +4,25 @@ __readme_file = open("README.md")
 __description = __readme_file.read()
 
 setuptools.setup(
-    name="coded-json",
+    name="cjson",
     version="1.0.0",
     description="Coded JSON files. Save your files with .cjson extension to unlock these features",
     long_description=__description,
     author="Shubhendu Shekhar Gupta",
     author_email="subhendushekhargupta@gmail.com",
-    packages=setuptools.find_packages(),
+    packages= [".", "utils/"],
     classifiers=[
+        "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
     ],
-    py_modules=["coded-json"],
+    include_package_data=True,
+    include_dirs=[ "utils" ],
+    py_modules=["cjson"],
     package_dir={ '':'cjson/src' }
 )
