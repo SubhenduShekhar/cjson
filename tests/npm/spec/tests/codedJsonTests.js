61656258a159293371585b163e7faf2fd2b2f679
SubhenduShekhar <42602572+SubhenduShekhar@users.noreply.github.com>
2023-06-11 22:13:12 +0530

    closes #16, #21 (#22)
    Co-authored-by: Gupta <shubhendushekhar.gupta@delta.com>

--- tests/npm/spec/tests/codedJsonTests.js
+++ tests/npm/spec/tests/codedJsonTests.js
@@ -3,8 +3,11 @@ const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
 
+/** Path to target.cjson */
 const cjsonfilePath = path.join(__dirname, "..", "..", "..", "\\test-files\\target.cjson");
+/** Path to source.json */
 const jsonfilePath = path.join(__dirname, "..", "..", "..", "\\test-files\\source.json");
+/** Path to pure.json */
 const pureJsonfilePath = path.join(__dirname, "..", "..", "..", "\\test-files\\pure.json");
 
 /**
@@ -37,10 +40,22 @@ describe("CJSON Test 1", () => {
 /**
  * Tests related to native JSON files
  */
-describe("CJSON Test 2", () => {
+describe("JSON Test 2", () => {
 
     it("I should be able to use isContentJson()", () => {
         var cjson = new Cjson(pureJsonfilePath);
         assert.equal(cjson.isContentJson(), true);
+    });
+
+    it("I should be able to parse jpath using `obj< Cjson >.json.parse(\"Valid.JPATH\")`", () => {
+        var cjson = new Cjson(cjsonfilePath);
+        var value = cjson.json.parse("source.quiz.sport.q1.question");
+        assert.equal(value, "Which one is correct team name in NBA?");
+    });
+
+    it("I should be able to parse full json using `obj< Cjson >.json.parse()`", () => {
+        var cjson = new Cjson(cjsonfilePath);
+        var value = cjson.json.parse();
+        assert.equal(value, cjson.deserialize());
     });
 });
