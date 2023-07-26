a3946310df5f22583b5fd52893879495f3cc2abf
Gupta <shubhendushekhar.gupta@delta.com>
2023-07-26 22:21:51 +0530

    pulled dotnet code

---
+++ dotnet/Coded-Json/Coded-Json/Support/Base.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coded_Json.Support
+{
+    public abstract class Base
+    {
+        protected String filePath;
+        protected String basePath;
+    }
+}
---
+++ dotnet/Coded-Json/Coded-Json/Support/Is.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coded_Json.Support
+{
+    public class Is : Decode
+    {
+        protected Boolean isImport(String lineItem)
+        {
+            if (lineItem.Contains("$import \""))
+                return true;
+            return false;
+        }
+    }
+}
