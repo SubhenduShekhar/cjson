e2090baf41b3e17842fd03aa325cde85d60ce7e9
SubhenduShekhar <42602572+SubhenduShekhar@users.noreply.github.com>
2023-08-05 22:58:54 +0530

    pulled dotnet code (#69)
    Co-authored-by: Gupta <shubhendushekhar.gupta@delta.com>

---
+++ dotnet/Coded-Json/Coded-Json/Support/Decode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coded_Json.Support
+{
+    public class Decode : Base
+    {
+        public String[] import(String item)
+        {
+            String importFilePath = item.Split("$import \"")[1].Split("\"")[0];
+            String baseDirectory = Directory.GetParent(this.filePath).FullName;
+            String importFullPath = Path.Combine(baseDirectory, importFilePath);
+            String fileText = File.ReadAllText(importFullPath);
+            return new String[] { importFilePath, fileText};
+        }
+    }
+}
---
+++ dotnet/Coded-Json/Coded-Json/Support/Int.cs
@@ -0,0 +1,21 @@
+﻿public class Target
+{
+    public Dictionary<string, string> Properties { get; set; }
+}
+
+public class Question
+{
+    public string question { get; set; }
+    public List<String> options { get; set; }
+    public String answer { get; set; }
+}
+public class Source
+{
+    public Dictionary<String, Dictionary<String, Question>> quiz { get; set; }
+}
+public class BaseObj
+{
+    public Source source { get; set; }
+    public Dictionary<String, String> target { get; set; }
+
+}
