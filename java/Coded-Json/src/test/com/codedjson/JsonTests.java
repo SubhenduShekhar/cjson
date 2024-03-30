95e26a32aed5735e3657a1425d8ed267b07a82c7
shubham kesri <shubhamkeshari90@gmail.com>
2024-03-30 12:18:23 +0530

    Issue 110-Code Coverage for Java (#139)
    * Issue-110 | Added invalid.json
    Issue-110 | Added Test cases
    
    * Issue-110 | Replaced stream with forEach operator
    
    * Issue-110 | Removed duplicate test

--- java/Coded-Json/src/test/com/codedjson/JsonTests.java
+++ java/Coded-Json/src/test/com/codedjson/JsonTests.java
@@ -2,11 +2,24 @@ package com.codedjson;
 
 import com.codedjson.exceptions.*;
 import com.codedjson.templates.Pure;
+import com.codedjson.templates.Questions;
 import com.codedjson.templates.Target;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
+import java.util.Collections;
+import java.util.List;
+import java.util.HashSet;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 public class JsonTests extends Base {
     @Test
@@ -16,6 +29,32 @@ public class JsonTests extends Base {
         Assertions.assertEquals(CJson.isContentJson(pureJsonfilePath), true, "Static IsContentJson check for file");
         Assertions.assertEquals(CJson.isContentJson(com.codedjson.utils.Base.read(pureJsonfilePath.toString())), true, "IsContentJson check for string content");
     }
+
+    @Test
+    public void iShouldBeAbleToUseIsContentJsonOnString() throws IllegalJsonType, AbsolutePathConstraintError, FileNotFoundException {
+        CJson<Pure> cJson = new CJson<>("{\n" +
+                "        \"types\": \"asd\",\n" +
+                "        \"fruit\": <fruit>" +
+                "}");
+
+        Assertions.assertTrue(cJson.isContentJson(), "String content parsed to CJson");
+    }
+
+    @Test
+    public void iShouldBeAbleToSeeIllegalJsonTypeException() throws IllegalJsonType, AbsolutePathConstraintError, FileNotFoundException {
+        String jsonString = "\n" +
+                "        \"types\": \"asd\",\n" +
+                "        \"fruit\": <fruit>" +
+                "}";
+
+        assertThrows(IllegalJsonType.class, ()-> new CJson<>(jsonString), "CJson initialisation check for invalid string content");
+    }
+
+    @Test
+    public void iShouldReceiveFalseFromIsContentJsonStaticMethod(){
+        assertFalse(CJson.isContentJson(invalidJsonFilePath));
+    }
+
     @Test
     public void iShouldBeAbleToParseJpath() throws FileNotFoundException, IllegalJsonType, AbsolutePathConstraintError, InvalidJPathError, UndeserializedCJSON {
         CJson<Target> cJson = new CJson<>(cjsonfilePath);
@@ -27,7 +66,7 @@ public class JsonTests extends Base {
     public void iShouldNotBeAbleToParseWithInvalidJpath() throws FileNotFoundException, IllegalJsonType, AbsolutePathConstraintError, UndeserializedCJSON {
         CJson<Target> cJson = new CJson<>(cjsonfilePath);
         cJson.deserialize(Target.class);
-        Assertions.assertThrows(InvalidJPathError.class, () -> {
+        assertThrows(InvalidJPathError.class, () -> {
             String value = cJson.parse("source.quiz.sport.q1.question").toString();
         });
     }
@@ -38,5 +77,38 @@ public class JsonTests extends Base {
         String value = cJson.parse().toString();
         System.out.println(value);
         Assertions.assertNotNull(value);
+    }
+
+    @Test
+    public void iShouldBeAbleToRetrieveAllTheKeys() throws IllegalJsonType, AbsolutePathConstraintError, FileNotFoundException, UndeserializedCJSON {
+        CJson<Pure> cjson = new CJson<>(pureJsonfilePath);
+        JsonObject pureJson = JsonParser.parseString(cjson.deserializeAsString()).getAsJsonObject();
+        List<String> keys = cjson.getAllKeys();
+        assertNotNull(keys);
+        assertEquals(pureJson.keySet().size(), keys.size());
+        assertEquals(pureJson.keySet(), new HashSet<>(Collections.singletonList(keys.get(0))));
+    }
+
+    @Test
+    public void iShouldBeAbleToRetrieveValueForTheKey() throws IllegalJsonType, AbsolutePathConstraintError, FileNotFoundException, UndeserializedCJSON {
+        CJson<Pure> cjson = new CJson<>(pureJsonfilePath);
+        Pure pure = cjson.deserialize(Pure.class);
+
+        JsonObject value = (JsonObject) cjson.getValueFromKey("quiz");
+
+        assertNotNull(value);
+        assertEquals(pure.quiz.size(),value.size());
+
+        List<Questions> questions = new ArrayList<>();
+
+        pure.quiz.keySet().stream().forEach(key->
+           questions.add(gson.fromJson(value.get(key).getAsJsonObject().get("q1"), Questions.class)));
+
+        assertEquals(pure.quiz.get("maths").get("q1").question, questions.get(0).question);
+        assertEquals(pure.quiz.get("maths").get("q1").options, questions.get(0).options);
+        assertEquals(pure.quiz.get("maths").get("q1").answer, questions.get(0).answer);
+        assertEquals(pure.quiz.get("sport").get("q1").question, questions.get(1).question);
+        assertEquals(pure.quiz.get("sport").get("q1").options, questions.get(1).options);
+        assertEquals(pure.quiz.get("sport").get("q1").answer, questions.get(1).answer);
     }
 }
