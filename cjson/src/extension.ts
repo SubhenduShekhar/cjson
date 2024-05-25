d6b46122ff63167c217d9c67786eb3aeda4e0315
Shubhendu Shekhar Gupta <subhendushekhargupta@gmail.com>
2024-05-25 20:12:47 +0530

    closes #156 @168 #169

--- cjson/src/extension.ts
+++ cjson/src/extension.ts
@@ -8,13 +8,16 @@ export function activate(context: vscode.ExtensionContext) {
 
 	let goToDefinition = Registers.registerGoToImportDefinitionCommand();
 
+	let a = Registers.registerDeseralizeAndPreviewCommand();
+
 	// disposable = cjsonRegisterDocumentLinkCommand();
 
 	// disposable = registerGoToDeclarationCommand();
 
 	context.subscriptions.push(
 		autoComplete,
-		goToDefinition
+		goToDefinition,
+		a
 	);
 }
 
